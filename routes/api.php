a3f4f745d3c891820b9bd3893476fe9b2f22397d
Dedi Nugroho <nugrohodedi62@gmail.com>
2021-01-03 18:54:32 +0700

    update route

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,23 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// menambahkan route untuk person
+
+// get data
+Route::get('/person','PersonController@all');
+
+// get data by id
+Route::get('/person/{id}','PersonController@show');
+
+// add data
+Route::post('/person','PersonController@store');
+
+// update data
+Route::put('/person/{id}','PersonController@update');
+
+// delete data
+Route::delete('/person/{id}','PersonController@delete');
