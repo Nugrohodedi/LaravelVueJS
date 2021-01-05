86f8ffa7806a42d772d5e937334ea12624d4f16f
Dedi Nugroho <nugrohodedi62@gmail.com>
2021-01-05 08:17:31 +0700

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * 
      *
      * @var array
      */
@@ -23,7 +23,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for arrays.
+     * 
      *
      * @var array
      */
@@ -33,7 +33,7 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be cast to native types.
+     * 
      *
      * @var array
      */
